7fdf6489a74e3f78f2a3428c2cbf2621320f5715
marcosfandrade <marcos.fellipe2211@gmail.com>
2022-05-21 23:23:46 -0300

    Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import './home.css';
-
+import { MdRecycling, MdOutlineMap } from "react-icons/md";
 import Navbar from '../../components/Navbar/Navbar';
 
 export function Home() {
@@ -7,7 +7,33 @@ export function Home() {
     return(
         <div className="container">
             <Navbar />
-            <p>This is home screen.</p>
+            <div className="home-container">
+                <div className="home-elements">
+                    <div className="top">
+                        <div className="top-text">
+                            <p>Olá!
+                            <br />
+                            <span className="name">Name</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mid">
+                        <div className="mid-text">
+                            <p>Bem vindo ao
+                            <br />
+                            <span className="app-name">Eco Future</span>, seu
+                            <br />
+                            app de reciclagem!</p>
+                        </div>
+                        <div className="mid-buttons">
+                            <div className="button"><MdRecycling /> Dicas de reciclagem</div>
+                            <div className="button"><MdOutlineMap /> Mapa</div>
+                            <div className="button">Nada</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+                
     )
 }
