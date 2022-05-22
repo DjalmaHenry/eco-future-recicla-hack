663fc005580289210366439101f3543cc352c946
Djalma Henrique <djalmahenry@outlook.com>
2022-05-22 13:36:40 -0300

    feat: add login with firebase and login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Login } from "./pages/Login/Login";
 import { Home } from "./pages/Home/Home";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/home" component={Home} />
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,46 @@
-import './login.css';
+import "./login.css";
+import googleIconImg from "../../assets/images/google-icon.svg";
+import ecoFutureImg from "../../assets/images/LogomarcaBranca.png";
+
+import { useHistory } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 export function Login() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-    return(
-        <div className="container">
-            <p>This is sign in screen.</p>
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push("/home");
+  }
+
+  return (
+    <div className="container">
+      <aside>
+        {/* <img className="img-aside" alt="Ilustração simbolizando perguntas e respostas" /> */}
+        <p className="p-aside">
+          Preserve o meio ambiente, descarte seu lixo de forma correta ouvindo
+          de quem entende!
+        </p>
+        <strong className="strong-aside">O que fazer? Como fazer?</strong>
+        <p className="p-aside">Descubra e comece agora com a Eco Future.</p>
+      </aside>
+      <main>
+        <div className="main-content">
+            <img src={ecoFutureImg} alt="Eco Future" className="img-login" />
+          <div className="show-mobile">
+            <strong className="strong-aside">O que reciclar? Como fazer?</strong>
+            <p className="p-aside">Descubra e comece agora com a Eco Future.</p>
+          </div>
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            <span>Entrar com o Google</span>
+          </button>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
